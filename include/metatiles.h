28502677340e4554a92cc213058df79fdd90283f
Shreyash Desai <shreyashsn.desai@gmail.com>
2026-02-07 23:03:01 +0530

    Create metatiles.h

---
+++ include/metatiles.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_METATILES_H
+#define GUARD_METATILES_H
+
+struct Metatile {
+    u16 layer1;   // background
+    u16 layer2;   // foreground
+    u16 layer3;   // new extra layer
+};
+
+#endif // GUARD_METATILES_H
